4f044af538acde10bfdbad90817917fa05cc7f88
Adam Whittingham <adam.whittingham@gmail.com>
2014-07-30 14:56:19 +0100

    Add test for when there are no site groups

--- spec/controllers/v1/sites_controller_spec.rb
+++ spec/controllers/v1/sites_controller_spec.rb
@@ -6,19 +6,28 @@ describe V1::SitesController do
     let(:govuk){'http://www.gov.uk'}
     let(:google){'http://www.google.com'}
 
-    before do
-      uk = FactoryGirl.create(:site_group, :uk)
-      uk.sites << FactoryGirl.create(:site, url: bbc)
-      uk.sites << FactoryGirl.create(:site, url: govuk)
+    context 'there are groups' do
+      before do
+        uk = FactoryGirl.create(:site_group, :uk)
+        uk.sites << FactoryGirl.create(:site, url: bbc)
+        uk.sites << FactoryGirl.create(:site, url: govuk)
 
-      us = FactoryGirl.create(:site_group, :us)
-      us.sites << FactoryGirl.create(:site, url: google)
+        us = FactoryGirl.create(:site_group, :us)
+        us.sites << FactoryGirl.create(:site, url: google)
+      end
+
+      it 'returns a sample of sites for each group' do
+        get "/sites", {}, {'version' => 1}
+        expect(json[:UK]).to match_array [bbc, govuk]
+        expect(json[:USA]).to match_array [google]
+      end
     end
 
-    it 'returns a sample of sites for each group' do
-      get "/sites", {}, {'version' => 1}
-      expect(json[:UK]).to match_array [bbc, govuk]
-      expect(json[:USA]).to match_array [google]
+    context 'there are no groups' do
+      it 'returns an empty hash' do
+        get "/sites", {}, {'version' => 1}
+        expect(json).to eq({})
+      end
     end
   end
 end
